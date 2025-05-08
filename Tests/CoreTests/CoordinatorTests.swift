aa3f5305eb694159ec3945fc2bb80f9f88abb8e7
Cristian Rojas <cristian.rojas@live.fr>
2025-05-09 01:50:31 +0200

    generateAndSaveCode() delivers error on generator error

--- Tests/CoreTests/CoordinatorTests.swift
+++ Tests/CoreTests/CoordinatorTests.swift
@@ -1,6 +1,7 @@
 // © 2025  Cristian Felipe Patiño Rojas. Created on 9/5/25.
 
 import XCTest
+import Core
 
 class CoordinatorTests: XCTestCase {
     
@@ -16,21 +17,28 @@ class CoordinatorTests: XCTestCase {
         func read(_ url: URL) throws -> String
     }
     
+    protocol Generator {
+        typealias Output = (generatedCode: String, output: Runner.Output)
+        func generateCode(from specs: String) async throws -> Output
+    }
+    
     class Coordinator {
         let reader: FileReader
-        
-        init(reader: FileReader) {
+        let generator: Generator
+        init(reader: FileReader, generator: Generator) {
             self.reader = reader
+            self.generator = generator
         }
         
         func generateAndSaveCode(specsFileURL: URL) async throws {
             let _ = try reader.read(specsFileURL)
+            let _ = try await generator.generateCode(from: "")
         }
     }
     
     func test_generateAndSaveCode_deliversErrorOnReaderError() async throws {
         let reader = FileReaderStub(result: .failure(anyError()))
-        let coordinator = Coordinator(reader: reader)
+        let coordinator = Coordinator(reader: reader, generator: GeneratorDummy())
         do {
             try await coordinator.generateAndSaveCode(specsFileURL: anyURL())
             XCTFail()
@@ -41,7 +49,7 @@ class CoordinatorTests: XCTestCase {
     
     func test_generateAndSaveCode_deliversNoErrorOnReaderSuccess() async throws {
         let reader = FileReaderStub(result: .success(""))
-        let coordinator = Coordinator(reader: reader)
+        let coordinator = Coordinator(reader: reader, generator: GeneratorDummy())
         do {
             try await coordinator.generateAndSaveCode(specsFileURL: anyURL())
         } catch {
@@ -49,10 +57,40 @@ class CoordinatorTests: XCTestCase {
         }
     }
     
+    func test_generateAndSaveCode_deliversErrorOnGeneratorError() async throws {
+        struct GeneratorStub: Generator {
+            let result: Result<Generator.Output, Error>
+            func generateCode(from specs: String) async throws -> Output {
+                try result.get()
+            }
+        }
+        
+        let generator = GeneratorStub(result: .failure(anyError()))
+        let coordinatior = Coordinator(reader: FileReaderDummy(), generator: generator)
+        do {
+            try await coordinatior.generateAndSaveCode(specsFileURL: anyURL())
+            XCTFail()
+        } catch {
+            XCTAssertEqual(error as NSError, anyError())
+        }
+    }
+    
     struct FileReaderStub: FileReader {
         let result: Result<String, Error>
         func read(_: URL) throws -> String {
             try result.get()
+        }
+    }
+    
+    struct FileReaderDummy: FileReader {
+        func read(_ url: URL) throws -> String {
+            ""
+        }
+    }
+    
+    struct GeneratorDummy: Generator {
+        func generateCode(from specs: String) async throws -> Output {
+            ("", output: ("", "", 0))
         }
     }
 }
