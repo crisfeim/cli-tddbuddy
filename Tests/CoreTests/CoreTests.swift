d0e04ff120d031b954f459dfa1885060e8cca297
Cristian Rojas <cristian.rojas@live.fr>
2025-05-08 22:58:36 +0200

    `generateCode` delivers error on runner error

--- Tests/CoreTests/CoreTests.swift
+++ Tests/CoreTests/CoreTests.swift
@@ -8,15 +8,29 @@ class CoreTests: XCTestCase {
         func send(systemPrompt: String, userMessages: [String]) async throws -> String
     }
     
+    protocol Runner {
+        func run(_ code: String) throws -> String
+    }
+    
     final class Generator {
         let client: Client
+        let runner: Runner
         
-        init(client: Client) {
+        init(client: Client, runner: Runner) {
             self.client = client
+            self.runner = runner
         }
         
         func generateCode(from specs: String) async throws -> String {
-            try await client.send(systemPrompt: "", userMessages: [])
+           let generated = try await client.send(systemPrompt: "", userMessages: [])
+           _ = try runner.run("")
+          return generated
+        }
+    }
+    
+    struct RunnerDummy: Runner {
+        func run(_ code: String) throws -> String {
+            ""
         }
     }
     
@@ -30,14 +44,14 @@ class CoreTests: XCTestCase {
     func test_generateCode_deliversCodeOnClientSuccess() async throws {
        
         let client = ClientStub(stub: .success("any generated code"))
-        let generator = Generator(client: client)
+        let generator = Generator(client: client, runner: RunnerDummy())
         let generated = try await generator.generateCode(from: "any specs")
         XCTAssertEqual(generated, "any generated code")
     }
     
     func test_generateCode_deliversErrorOnClientError() async throws {
         let client = ClientStub(stub: .failure(NSError(domain: "any error", code: 0)))
-        let generator = Generator(client: client)
+        let generator = Generator(client: client, runner: RunnerDummy())
         do {
             let _ = try await generator.generateCode(from: "any specs")
             XCTFail()
@@ -46,5 +60,26 @@ class CoreTests: XCTestCase {
         }
     }
     
-   
+    func test_generateCode_deliversErrorOnRunnerError() async throws {
+        struct DummyClient: Client {
+            func send(systemPrompt: String, userMessages: [String]) async throws -> String {
+                ""
+            }
+        }
+        struct RunnerStub: Runner {
+            let stub: Result<String, Error>
+            func run(_ code: String) throws -> String {
+                try stub.get()
+            }
+            
+        }
+        let runner = RunnerStub(stub: .failure(NSError(domain: "any error", code: 0)))
+        let generator = Generator(client: DummyClient(), runner: runner)
+        do {
+            let _ = try await generator.generateCode(from: "any specs")
+            XCTFail()
+        } catch {
+            XCTAssertEqual(error as NSError, NSError(domain: "any error", code: 0))
+        }
+    }
 }
