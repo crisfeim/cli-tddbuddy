d49b1d4ea4c9a67cc8467153d59dbf1b37fc4735
Cristian Rojas <cristian.rojas@live.fr>
2025-05-08 22:36:39 +0200

    Generator delivers generated code on client success

--- Sources/tddbuddy/TddBuddy.swift
+++ Sources/tddbuddy/TddBuddy.swift
@@ -8,7 +8,8 @@ import ArgumentParser
 
 @main
 struct TddBuddy: ParsableCommand {
-    mutating func run() throws {
+    mutating func run() async throws {
         print("Hello, world!")
     }
 }
+
--- Tests/CoreTests/CoreTests.swift
+++ Tests/CoreTests/CoreTests.swift
@@ -3,9 +3,35 @@
 import XCTest
 
 class CoreTests: XCTestCase {
-    func test() {
-        XCTExpectFailure {
-            XCTFail()
+    
+    protocol Client {
+        func send(systemPrompt: String, userMessages: [String]) async throws -> String
+    }
+    
+    final class Generator {
+        let client: Client
+        
+        init(client: Client) {
+            self.client = client
+        }
+        
+        func generateCode(from specs: String) async throws -> String {
+            try await client.send(systemPrompt: "", userMessages: [])
         }
     }
+    
+    func test_generate_deliversCodeOnClientSuccess() async throws {
+        struct ClientStub: Client {
+            let stub: String
+            func send(systemPrompt: String, userMessages: [String]) async throws -> String {
+                return stub
+            }
+        }
+        
+        let client = ClientStub(stub: "any generated code")
+        let generator = Generator(client: client)
+        let generated = try await generator.generateCode(from: "any specs")
+        XCTAssertEqual(generated, "any generated code")
+    }
+   
 }
