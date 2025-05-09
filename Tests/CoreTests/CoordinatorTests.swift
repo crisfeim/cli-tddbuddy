30480510e54773b8bcf486399fc914a48aa3d8e9
Cristian Rojas <cristian.rojas@live.fr>
2025-05-09 02:32:02 +0200

    Add iteration tests

--- Tests/CoreTests/CoordinatorTests.swift
+++ Tests/CoreTests/CoordinatorTests.swift
@@ -3,6 +3,35 @@
 import XCTest
 import Core
 
+class IteratorTests: XCTestCase {
+    
+    class Iterator {
+        let maxCount: Int
+        let action: () async throws -> Void
+        
+        init(maxCount: Int, action: @escaping () async throws -> Void) {
+            self.maxCount = maxCount
+            self.action = action
+        }
+        
+        func start() async throws {
+            var currentCount = 0
+            while currentCount < maxCount {
+                currentCount += 1
+                try await action()
+            }
+        }
+    }
+    
+    func test_iterator() async throws {
+        var currentIteration = 0
+        let action = { currentIteration += 1 }
+        let iterator = Iterator(maxCount: 5, action: action)
+        try await iterator.start()
+        XCTAssertEqual(currentIteration, 5)
+    }
+}
+
 class CoordinatorTests: XCTestCase {
     
     func anyURL() -> URL {
