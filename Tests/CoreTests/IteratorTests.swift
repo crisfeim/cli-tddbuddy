2c46f1838b1ce626533df7a767cb0b9e1058ffb9
Cristian Rojas <cristian.rojas@live.fr>
2025-05-09 03:19:47 +0200

    Add condition to break loop

--- Tests/CoreTests/IteratorTests.swift
+++ Tests/CoreTests/IteratorTests.swift
@@ -6,8 +6,8 @@ import Core
 
 class Iterator {
     var currentCount = 0
-    func start(maxCount: Int, action: () async throws -> Void) async throws {
-        while currentCount < maxCount {
+    func start(maxCount: Int, until condition: () -> Bool, action: () async throws -> Void) async throws {
+        while currentCount < maxCount && !condition() {
             currentCount += 1
             try await action()
         }
@@ -17,10 +17,16 @@ class Iterator {
 class IteratorTests: XCTestCase {
     
     func test_iterator_iteratesNtimes() async throws {
-        var currentIteration = 0
-        let action = { currentIteration += 1 }
         let iterator = Iterator()
-        try await iterator.start(maxCount: 5, action: action)
-        XCTAssertEqual(currentIteration, 5)
+        try await iterator.start(maxCount: 5, until: neverFullfillsCondition, action: {})
+        XCTAssertEqual(iterator.currentCount, 5)
     }
+    
+    func test_iterator_stopsWhenConditionIsMet() async throws {
+        let iterator = Iterator()
+        try await iterator.start(maxCount: 5, until: { iterator.currentCount == 1 }, action: {})
+        XCTAssertEqual(iterator.currentCount, 1)
+    }
+    
+    private func neverFullfillsCondition() -> Bool { false }
 }
