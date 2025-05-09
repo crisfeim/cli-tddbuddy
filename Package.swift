75a539968a92dd75e5b16b73e9270a11cdb1ba9e
Cristian Rojas <cristian.rojas@live.fr>
2025-05-09 05:34:10 +0200

    Move ollama client to production

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "TddBuddy",
+    platforms: [.macOS(.v12)],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
     ],
--- Tests/CoreE2ETests/OllamaClientTests.swift
+++ Tests/CoreE2ETests/OllamaClientTests.swift
@@ -2,44 +2,7 @@
 
 import XCTest
 import Foundation
-
-struct OllamaClient {
-    private let model = "llama3.2"
-    private let url = "http://localhost:11434/api/chat"
-    func send(userMessage: String) async throws -> String {
-        let url = URL(string: url)!
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.httpBody = try makeBody(userMessage)
-        
-        let (data, httpResponse) = try await URLSession.shared.data(for: request)
-        
-        guard let httpResponse = httpResponse as? HTTPURLResponse, httpResponse.statusCode == 200 else {
-            throw URLError(.badServerResponse)
-        }
-        
-        return try JSONDecoder().decode(Response.self, from: data).message.content
-    }
-    
-    private func makeBody(_ userMessage: String) throws -> Data {
-        try JSONSerialization.data(withJSONObject: [
-            "model": model,
-            "messages": [
-                ["role": "user", "content": userMessage]
-            ],
-            "stream": false
-        ], options: [])
-    }
-
-    struct Response: Decodable {
-        let message: Message
-        // MARK: - Message
-        struct Message: Decodable {
-            let role: String
-            let content: String
-        }
-    }
-}
+import Core
 
 class OllamaClientTests: XCTestCase {
     
